c91d815e9cd24a930170062acd792a6e262fab63
KarenNgala <ngalalalu@gmail.com>
2020-07-13 00:33:37 +0300

    07: Create model and unit test

---
+++ app/models.py
@@ -0,0 +1,11 @@
+class Pitch:
+    '''
+    '''
+    def __init__(self, text, category, author):
+        '''
+        '''
+        self.text=text
+        self.category=category
+        self.author=author
+
+        
