3cb2e6fc09dd9c9512f8340a7f986708b43409e6
KarenNgala <ngalalalu@gmail.com>
2020-07-12 22:21:14 +0300

    Add views

--- app/views.py
+++ app/views.py
@@ -4,3 +4,11 @@ from flask import render_template
 @app.route('/')
 def landing_page():
     return render_template('index.html')
+
+@app.route('/pitches')
+def home():
+    return render_template('pitches.html')
+
+@app.route('/pitches/<category>')
+def categories(category):
+    return render_template('categories.html')
