f7150e193f1dec127afc79003e17e459c374238c
KarenNgala <ngalalalu@gmail.com>
2020-07-13 13:21:06 +0300

    09: Initialize psql db

--- config.py
+++ config.py
@@ -4,6 +4,7 @@ class Config:
     '''
     '''
     SECRET_KEY=os.environ.get('SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI=os.environ.get('SQLALCHEMY_DATABASE_URI')
 
 class DevelopmentConfig(Config):
     '''
