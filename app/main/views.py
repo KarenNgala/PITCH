0b23aee36ab1d9783d8746fdf5e6580aead93910
KarenNgala <ngalalalu@gmail.com>
2020-07-15 02:54:32 +0300

    11. Add user login, logout and signup functionalities

--- app/main/views.py
+++ app/main/views.py
@@ -1,5 +1,6 @@
 from . import main
 from flask import render_template
+from flask_login import login_required
 
 @main.route('/')
 def landing_page():
@@ -12,3 +13,8 @@ def home():
 @main.route('/pitches/<category>')
 def categories(category):
     return render_template('categories.html')
+
+@main.route('/comments')
+@login_required
+def comments():
+    return render_template('comments.html')
--- config.py
+++ config.py
@@ -4,17 +4,17 @@ class Config:
     '''
     '''
     SECRET_KEY=os.environ.get('SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI='postgresql+psycopg2://lalu:0@localhost/pitch'
-
-class DevelopmentConfig(Config):
-    '''
-    '''
-    DEBUG=True
+    SQLALCHEMY_DATABASE_URI=os.environ.get('SQLALCHEMY_DATABASE_URI')
 
 class ProductionConfig(Config):
     '''
     '''
     pass
+
+class DevelopmentConfig(Config):
+    '''
+    '''
+    DEBUG=True
 
 config_options={
     'dev': DevelopmentConfig,
